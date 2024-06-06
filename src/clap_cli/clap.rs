558432a45c12477b4d3d169de4b8940eea251185
bryanyi <bryanyi1994@gmail.com>
2024-06-06 08:00:59 -0700

    starting documentation

--- src/clap_cli/clap.rs
+++ src/clap_cli/clap.rs
@@ -2,10 +2,14 @@ use clap::ArgAction::SetTrue;
 use clap::Parser;
 
 #[derive(Parser)]
+#[command(version, about)]
 pub struct Cli {
+    /// Full URL link to a single YouTube Video
     pub url: String,
+    /// Optional - Full path to the output directory. Can use '~'
     #[arg(long, short = 'o')]
     pub output_dir: Option<String>,
+    /// Optional - Download only the mp3 audio file of the YouTube video
     #[arg(long, short = 'a', action = SetTrue, default_value_t = false)]
     pub audio_only: bool,
 }
